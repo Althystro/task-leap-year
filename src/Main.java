a7130bc33cb872adbc1f47c9b3df7a4a4876d304
Abdulwahab-Alawadhi <abdulwahab.alawadhi56@gmail.com>
2024-11-06 18:56:04 +0300

    done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        Scanner year = new Scanner(System.in);
+        System.out.println("Please enter: ");
+        int LeapYear = year.nextInt();
+
+        if (LeapYear % 4 == 0 && LeapYear % 100 != 0 || LeapYear % 400 == 0) {
+            System.out.println("It's a leap year");
+        } else {
+            System.out.println("It's not a Leap");
+
+        }
+
+        year.close();
+
     }
 }
